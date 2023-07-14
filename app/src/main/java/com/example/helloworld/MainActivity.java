923241039638757385897123e6db1f3e814cbc36
PhanTien234 <minhtienp328@gmail.com>
2023-07-14 11:54:37 +0700

    create-gui

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView courseName;
+    EditText nameInput;
+    Button pressMeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        courseName = findViewById(R.id.course_name);
-        courseName.setText(R.string.course_name); // Get value of course_name in strings.xml
+        nameInput = findViewById(R.id.name_input);
+        pressMeBtn = findViewById(R.id.press_me);
+
+        pressMeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nameInput.setText("The button was pressed ...");
+            }
+        });
     }
 }
